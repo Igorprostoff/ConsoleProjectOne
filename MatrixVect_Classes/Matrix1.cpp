8e406da5780f08daaa4e1079659a1328845aeaa4
Игорь Простов <igorprostoff@gmail.com>
2017-11-21 18:07:16 +0300

    Добавил проект для векторно-матричных операций в классах

---
+++ MatrixVect_Classes/Matrix1.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Matrix1.h"
+
+
+Matrix1::Matrix1()
+{
+}
+
+
+Matrix1::~Matrix1()
+{
+}
---
+++ MatrixVect_Classes/Matrix1.h
@@ -0,0 +1,9 @@
+#pragma once
+class Matrix1
+{
+public:
+	Matrix1();
+	~Matrix1();
+	int array[3][3];
+};
+
