305bdc59f9100f5c69b79bd71b8d9133d14ce927
Igor Prostov <igorprostoff@gmail.com>
2017-11-23 23:36:17 +0300

    Попытка реализовать непонятное ТЗ по классам

---
+++ MatrixVect_Classes/Matrix.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+class Matrix
+{
+public: 
+	    int l1;
+
+		int l2;
+
+		int l3;
+
+		int l4;
+
+		int l5;
+
+		int l6;
+
+		int g2;
+
+		int g1;
+
+		int ** matrixVectDyn = new int *[g1];
+
+		int ** matrixDyn = new int *[l1];
+
+		int ** matrix1Dyn = new int *[l3];
+
+		int ** matrix2Dyn = new int *[l5];
+		
+	};
+
+
+
+	
+
+
+
