25964655dac1006251e0387819cc8630ca9f663b
Igor Prostov <igorprostoff@gmail.com>
2017-12-30 17:53:03 +0300

    Исправлено ДЗ

--- MatrixVect_Classes/Matrix.cpp
+++ MatrixVect_Classes/Matrix.cpp
@@ -9,7 +9,7 @@ void Matrix::vyvod_mat(Matrix Matr1)
 	cout << endl;
 	cout << endl;
 	int l1 = Matr1.l1;
-		int l2 = Matr1.l2;
+	int l2 = Matr1.l2;
 	for (int i = 0; i < l1; i++) {
 		for (int j = 0; j < l2; j++)
 		{
@@ -20,33 +20,7 @@ void Matrix::vyvod_mat(Matrix Matr1)
 	cout << std::endl;
 }
 
-void Matrix::matrix_slozh(Matrix Matr1, Matrix Matr2, Matrix & Matr3)
-{
-	int l3 = Matr1.l1;
-	int l5 = Matr2.l1;
-	int l4 = Matr1.l2;
-	int l6 = Matr2.l2;
-
-	if ((l3 == l5) && (l4 == l6)) 
-	{
-		int l1 = l3;
-		int l2 = l4;
-		for (int i = 0; i < l1; i++) 
-		{
-			for (int j = 0; j < l2; j++) 
-			{
-				Matr3.matrixDyn[i][j] = 0;
-
-				Matr3.matrixDyn[i][j] = Matr1.matrixDyn[i][j] + Matr2.matrixDyn[i][j];
-
-			}
-		}
-		
-	}
-
-}
-
-void Matrix::matrix_vych(Matrix Matr1, Matrix Matr2, Matrix & Matr3)
+Matrix Matrix::matrix_slozh(Matrix Matr1, Matrix Matr2)
 {
 	int l3 = Matr1.l1;
 	int l5 = Matr2.l1;
@@ -55,110 +29,94 @@ void Matrix::matrix_vych(Matrix Matr1, Matrix Matr2, Matrix & Matr3)
 
 	if ((l3 == l5) && (l4 == l6))
 	{
+		Matrix Otv(Matr1.l1, Matr1.l2);
 		int l1 = l3;
 		int l2 = l4;
 		for (int i = 0; i < l1; i++)
 		{
 			for (int j = 0; j < l2; j++)
 			{
-				Matr3.matrixDyn[i][j] = 0;
+				Otv.matrixDyn[i][j] = 0;
 
-				Matr3.matrixDyn[i][j] = Matr1.matrixDyn[i][j] - Matr2.matrixDyn[i][j];
+				Otv.matrixDyn[i][j] = Matr1.matrixDyn[i][j] + Matr2.matrixDyn[i][j];
 
 			}
 		}
-	
+
+		return Otv;
 	}
 
 }
 
-
-void Matrix::matrix_umnozh(Matrix Matr1, Matrix Matr2, Matrix & Matr3) 
+Matrix Matrix::matrix_vych(Matrix Matr1, Matrix Matr2)
 {
-		int l3 = Matr1.l1;
+	int l3 = Matr1.l1;
+	int l5 = Matr2.l1;
+	int l4 = Matr1.l2;
+	int l6 = Matr2.l2;
 
-		int l4 = Matr1.l2;
-
-		int l5 = Matr2.l1;
-
-		int l6 = Matr2.l2;
-		
-
-	if ((l3 == l6) && (l3 != 1) && (l5 !=1)) 
+	if ((l3 == l5) && (l4 == l6))
 	{
-		Matrix Otv(Matr1.l1, Matr2.l2);
-		
-		for (int i = 0; i < l3; i++) 
+		Matrix Otv(Matr1.l1, Matr1.l2);
+		int l1 = l3;
+		int l2 = l4;
+		for (int i = 0; i < l1; i++)
 		{
-			for (int j = 0; j < l6; j++)
+			for (int j = 0; j < l2; j++)
 			{
 				Otv.matrixDyn[i][j] = 0;
 
-				for (int k = 0; k < l5; k++) 
-				{
+				Otv.matrixDyn[i][j] = Matr1.matrixDyn[i][j] - Matr2.matrixDyn[i][j];
 
-					Otv.matrixDyn[i][j] += (Matr1.matrixDyn[i][k] * Matr2.matrixDyn[k][j]);
-
-				
-				}
-			
 			}
 		}
+
+		return Otv;
 	}
 
-	if ((l3 == 1) && (l5 == 1)) 
-	{
-
-		Matrix Otv(1, 1);
-
-		int * vectDyn = new int [Matr2.l2];
-		for (int i = 0; i < l2; i++) {
-			vectDyn[i] = Matr2.matrixDyn[0][i];
-		}
-
-		int g1 = l3;
-		int g2 = l5;
-		if (g1 == g2) {
-			int	otvet = 0;
-			for (int i = 0; i < g1; i++) {
-
-				otvet = otvet + Matr1.matrixDyn[0][i] * vectDyn[i];
-			}
-			Otv.matrixDyn[0][0] = otvet;
-			for (int i = 1; i < Matr3.l2; i++) 
-			{
-				Otv.matrixDyn[0][i] = 0;
-			}
-		}
-	}
 }
 
 
+Matrix Matrix::matrix_umnozh(Matrix Matr1, Matrix Matr2)
+{
+	int l3 = Matr1.l1;
+
+	int l4 = Matr1.l2;
+
+	int l5 = Matr2.l1;
+
+	int l6 = Matr2.l2;
+
+	if (l3 == l6)
+	{
+		Matrix Otv(Matr1.l1, Matr2.l2);
+
+		for (int i = 0; i < l3; i++)
+		{
+			for (int j = 0; j < l3; j++)
+			{
+				Otv.matrixDyn[i][j] = 0;
+
+				for (int k = 0; k < l4; k++)
+				{
+					Otv.matrixDyn[i][j] += (Matr1.matrixDyn[i][k] * Matr2.matrixDyn[k][j]);
+				}
+
+			}
+		}
+		return Otv;
+	}
+}
 
 Matrix::Matrix()
 {
-	
+
 	cout << "enter matrix length ";
 	cin >> l1;
+	ll1 = l1;
 	cout << "enter matrix height ";
 	cin >> l2;
-	matrixDyn = new int *[l1];
-	for (int i = 0; i < l1; i++)
-	{
-		matrixDyn[i] = new int[l2];
-
-	}
-
-	for (int i = 0; i < l1; i++) {
-		for (int j = 0; j < l2; j++)
-		{
-			matrixDyn[i][j] = 2;
-		}
-	}
-}
-
-Matrix::Matrix(int l1, int l2)
-{
+	ll2 = l2;
 	matrixDyn = new int *[l1];
 	for (int i = 0; i < l1; i++)
 	{
@@ -173,18 +131,42 @@ Matrix::Matrix(int l1, int l2)
 		}
 	}
 }
-Matrix::Matrix(int **matrix1Dyn, int l3, int l4) 
+
+Matrix::Matrix(int lll1, int lll2)
+{
+	l1 = lll1;
+	l2 = lll2;
+	ll2 = l2;
+	ll1 = l1;
+
+	matrixDyn = new int *[l1];
+	for (int i = 0; i < l1; i++)
+	{
+		matrixDyn[i] = new int[l2];
+
+	}
+
+	for (int i = 0; i < l1; i++) {
+		for (int j = 0; j < l2; j++)
+		{
+			matrixDyn[i][j] = 0;
+		}
+	}
+}
+Matrix::Matrix(int **matrix1Dyn, int l3, int l4)
 {
 	l1 = l3;
+	ll1 = l1;
 	l2 = l4;
-	matrixDyn= new int *[l1];
-	for (int i = 0; i <l3 ; i++)
+	ll2 = l2;
+	matrixDyn = new int *[l1];
+	for (int i = 0; i <l3; i++)
 	{
 		matrixDyn[i] = new int[l4];
 
 	}
-	
-	for (int i=0; i<l3;i++)
+
+	for (int i = 0; i<l3; i++)
 	{
 		for (int j = 0; j < l4; j++) {
 			matrixDyn[i][j] = matrix1Dyn[i][j];
@@ -195,10 +177,7 @@ Matrix::Matrix(int **matrix1Dyn, int l3, int l4)
 
 Matrix::~Matrix()
 {
-	/*for (int i = 0; i < l1; i++)
-	{
-		delete matrixDyn[i];
-	}
-	delete[] matrixDyn;*/
+
 }
+
 
--- MatrixVect_Classes/Matrix.h
+++ MatrixVect_Classes/Matrix.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <iostream>
-using namespace std;
 
+using namespace std;
+//template <class Type>
 class Matrix
 {
 private:
@@ -10,27 +11,29 @@ private:
 
 	int l2;
 
-
-		
-public: 
+public:
 	int ** matrixDyn;
-	int ll1 = l1;
-	int ll2 = l2;
-		Matrix(int **matrix1Dyn, int l3, int l4);
-		Matrix(int l1, int l2);
-		Matrix();
-		void vyvod_mat(Matrix Matr);
-		
-		void matrix_slozh(Matrix Matr1, Matrix Matr2, Matrix & Matr3);
 
-		void matrix_umnozh(Matrix Matr1, Matrix Matr2, Matrix & Matr3);
+	int ll1;
 	
-		void matrix_vych(Matrix Matr1, Matrix Matr2, Matrix & Matr3);
+	int ll2;
+
+	Matrix(int **matrix1Dyn, int l3, int l4);
+	Matrix(int l1, int l2);
+	Matrix();
+
+	void vyvod_mat(Matrix Matr);
+
+	Matrix matrix_slozh(Matrix Matr1, Matrix Matr2);
+
+	Matrix matrix_umnozh(Matrix Matr1, Matrix Matr2);
+
+	Matrix matrix_vych(Matrix Matr1, Matrix Matr2);
 
 
-		~Matrix();
+	~Matrix();
 
-	};
+};
 
 
 
